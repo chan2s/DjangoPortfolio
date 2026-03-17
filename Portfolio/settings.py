750f28e462b95ba29c49027e17fde96422595619
Cristian Cutiee <paulaspera2010@gmail.com>
2026-03-17 20:48:34 +0800

    STATIC_ROOT added

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR =[
     os.path.join(BASE_DIR, 'frontEnd/static'),
 ]
