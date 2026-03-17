69a42dee3918c3d31ac19b753e1179875792bae3
Cristian Cutiee <paulaspera2010@gmail.com>
2026-03-17 20:53:18 +0800

    Fix static files and update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -116,9 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'  # URL for static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic will collect
-STATICFILES_DIRS = [  # Folders where Django will look for static files
+STATIC_URL = '/static/'
+
+# Folder where 'collectstatic' will copy all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Folders to look for additional static files
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontEnd/static'),
 ]
 CORS_ALLOWED_ORIGINS = [
